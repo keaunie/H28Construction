d02f125f87469bccac660424f3c55161a8bf2ef9
Kyooo <keaunieting@gmail.com>
2025-10-27 19:29:44 +0800

    Updated the socials links (Instagram & Facebook)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Linkedin, Twitter, Facebook } from 'lucide-react';
+import { Facebook, Instagram } from 'lucide-react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 const Footer = () => {
@@ -118,25 +118,18 @@ const Footer = () => {
             <span className="font-semibold text-lg mb-4 block">Connect</span>
             <div className="flex space-x-4">
               <a
-                href="#"
-                className="w-10 h-10 border border-white/20 flex items-center justify-center hover:bg-yellow-400 hover:text-black transition-all duration-300"
-                aria-label="LinkedIn"
-              >
-                <Linkedin className="w-5 h-5" strokeWidth={1.5} />
-              </a>
-              <a
-                href="#"
-                className="w-10 h-10 border border-white/20 flex items-center justify-center hover:bg-yellow-400 hover:text-black transition-all duration-300"
-                aria-label="Twitter"
-              >
-                <Twitter className="w-5 h-5" strokeWidth={1.5} />
-              </a>
-              <a
-                href="#"
+                href="https://www.facebook.com/profile.php?id=61582925968352"
                 className="w-10 h-10 border border-white/20 flex items-center justify-center hover:bg-yellow-400 hover:text-black transition-all duration-300"
                 aria-label="Facebook"
               >
                 <Facebook className="w-5 h-5" strokeWidth={1.5} />
+              </a>
+              <a
+                href="https://www.instagram.com/h28construction/"
+                className="w-10 h-10 border border-white/20 flex items-center justify-center hover:bg-yellow-400 hover:text-black transition-all duration-300"
+                aria-label="Instagram"
+              >
+                <Instagram className="w-5 h-5" strokeWidth={1.5} />
               </a>
             </div>
           </div>
